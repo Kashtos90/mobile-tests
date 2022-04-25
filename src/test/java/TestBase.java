eae4583d42f3d6f225196212ab21ec2967271b08
Sergei Kashtuev <29061990a@gmail.com>
2022-04-25 21:24:44 +0700

    added fix

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -30,11 +30,9 @@ public class TestBase {
     @AfterEach
     public void afterEach() {
         String sessionId = getSessionId();
-
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
-
-        closeWebDriver();
         Attach.video(sessionId);
+        closeWebDriver();
     }
 }
