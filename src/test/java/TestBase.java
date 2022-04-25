7e7cbe449c7a7a8b278ae2e6429686488974c00a
Sergei Kashtuev <29061990a@gmail.com>
2022-04-25 20:00:40 +0700

    added drivers and helpers

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Configuration;
+import drivers.BrowserstackMobileDriver;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.Attach.getSessionId;
 
 public class TestBase {
 
