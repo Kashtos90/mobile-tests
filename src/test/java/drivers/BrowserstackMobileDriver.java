c4b6a3f2ca79455a2ae0123c11a31d24e21cb536
Sergei Kashtuev <29061990a@gmail.com>
2022-05-04 23:28:40 +0700

    added fixes

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -12,6 +12,14 @@ import java.net.URL;
 
 public class BrowserstackMobileDriver implements WebDriverProvider {
 
+    public static URL getBrowserstackUrl() {
+        try {
+            return new URL(Credentials.configBro.url());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public WebDriver createDriver(Capabilities caps) {
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
@@ -28,13 +36,5 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
         mutableCapabilities.setCapability("name", "first_test");
 
         return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
-    }
-
-    public static URL getBrowserstackUrl() {
-        try {
-            return new URL(Credentials.configBro.url());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
